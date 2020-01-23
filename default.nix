ac5cd295900deef3c584c7738da1ba5dbd91f747
waddlaw <ingronze@gmail.com>
2020-01-23 20:55:48 +0900

    update

--- default.nix
+++ default.nix
@@ -1,11 +1,5 @@
-{ pkgs ? import <nixpkgs> {} }:
-let
-  result = pkgs.haskell.packages.ghcjs.callCabal2nix "miso" (pkgs.fetchFromGitHub {
-   sha256 = "078lhm9aqfmpsk1793ijal7kkpwzlnaa7kk517dannci696k94fd";
-   rev = "0cb882b48cc36d031d06696b5a1af84cdb0b5d07";
-   owner = "haskell-miso";
-   repo = "miso";
-}) {};
-in pkgs.haskell.packages.ghcjs.callPackage ./hs-flatris.nix {
-  miso = result;
-}
+with (import (builtins.fetchTarball {
+  url = "https://github.com/dmjio/miso/archive/561ffad.tar.gz";
+  sha256 = "1wwzckz2qxb873wdkwqmx9gmh0wshcdxi7gjwkba0q51jnkfdi41";
+}) {});
+pkgs.haskell.packages.ghcjs.callCabal2nix "flatris" ./. {}
